16039b2135a33506b8066355819c3276122f6e12
Eduardo Tongson <propolice@gmail.com>
2016-04-09 12:04:13 +0800

    Initial commit.

---
+++ aux/rules.mk
@@ -0,0 +1,47 @@
+$(LUAC_T):
+	$(ECHOT) [CC] $@
+	$(CC) -o $@ -DMAKE_LUAC $(DEFINES) $(INCLUDES) $(CCWARN) $(ONE).c -lm
+
+$(LUA_T):
+	$(ECHOT) [CC] $@
+	$(CC) -o $@ -DMAKE_LUA $(luaDEFINES) $(INCLUDES) $(CCWARN) $(CFLAGS) $(CCOPT) $(ONE).c -lm
+
+$(LUA_O): $(BUILD_DEPS) $(LUA_T)
+	$(ECHOT) [CC] $@
+	$(CC) -o $@ -c -DMAKE_LIB $(DEFINES) $(luaDEFINES) $(INCLUDES) $(CCWARN) $(CFLAGS) $(CCOPT) $(ONE).c
+
+$(LUA_A): $(LUA_O)
+	$(ECHOT) [AR] $@
+	$(AR) $(ARFLAGS) $@ $< >/dev/null 2>&1
+	$(RANLIB) $@
+
+$(EXE): $(LUA_A) $(CLUA_MODS)
+	$(ECHOT) [CP] $(LUA_MODS)
+	for f in $(LUA_MODS); do cp $(MODULES_P)/$$f .; done
+	for d in $(VENDOR_SUBDIRS); do cp -R $(MODULES_P)/$$d .; done
+	$(ECHOT) [LN] $(MAIN)
+	$(LUA_T) $(LUASTATIC) $(MAIN) $(LUA_MODS) $(VENDOR_DEPS) $(CLUA_MODS) $(LUA_A) $(INCLUDES) $(CCWARN) $(CFLAGS) $(CCOPT) $(LDFLAGS) 2>&1 >/dev/null
+
+clean: $(CLEAN)
+	$(ECHO) "Cleaning up..."
+	$(RM) $(RMFLAGS) $(LUA_O) $(LUA_T) $(LUAC_T) $(EXE) $(LUA_A) $(MAIN).c $(LUA_MODS)
+	$(RMRF) $(VENDOR_SUBDIRS)
+	$(ECHO) "Done!"
+
+print-%: ; @echo $*=$($*)
+
+vprint-%:
+	@echo '$*=$($*)'
+	@echo ' origin = $(origin $*)'
+	@echo ' flavor = $(flavor $*)'
+	@echo ' value = $(value $*)'
+
+has-%:
+	@command -v "${*}" >/dev/null 2>&1 || { \
+		echo "Missing build-time dependency: ${*}"; \
+		exit -1; \
+	}
+
+.PHONY: all clean sections exe print-% vprint-% has-% %LUA
+
+
---
+++ aux/std.mk
@@ -0,0 +1,30 @@
+all: $(EXE)
+	
+AUX_P= aux
+MODULES_P= vendor/lua
+ONE= $(AUX_P)/one
+LUASTATIC= aux/luastatic.lua
+LUAC_T= luac
+LUA_O= $(ONE).o
+LUA_A= liblua.a
+LUA_T= lua
+LUACFLAGS?= -s
+ECHO= @printf '%s\n'
+ECHON= @printf '%s'
+ECHOT= @printf ' %s\t%s\n'
+CP= cp
+STRIP= strip
+STRIPFLAGS= --strip-all
+RM= rm
+RMFLAGS= -f
+RMRF= rm -rf
+
+_rest= $(wordlist 2,$(words $(1)),$(1))
+_lget= $(firstword lib/$(1))/Makefile $(if $(_rest),$(call _lget,$(_rest)),)
+_vget= $(firstword vendor/c/$(1))/Makefile $(if $(_rest),$(call _vget,$(_rest)),)
+SRC_P= aux/lua
+INCLUDES:= -I$(SRC_P) -Iinclude -I$(AUX_P)
+CLUA_MODS+= $(foreach m, $(VENDOR_C), $m.a)
+CLUA_MODS+= $(foreach m, $(LIB), $m.a)
+LUA_MODS+= $(foreach m, $(VENDOR_LUA), $m.lua)
+BUILD_DEPS= has-$(CC) has-$(RANLIB) has-$(LD) has-$(AR) has-$(STRIP) has-$(RM) has-$(CP)
